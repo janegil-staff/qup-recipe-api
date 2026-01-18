bf5454d660d464fd2052452f31ae205297d0eed7
Jan Egil Staff Øvretveit <janegil-staff@Mac.lan>
2026-01-18 16:12:23 +0100

    Added health endpont

--- src/app.js
+++ src/app.js
@@ -3,8 +3,12 @@ import routes from "./routes/index.js";
 
 const app = express();
 
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
 
 app.use("/api", routes);
 
