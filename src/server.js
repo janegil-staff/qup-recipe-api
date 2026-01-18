62e7240239bf1474762b5b12cbf257180af8bdd7
Jan Egil Staff Øvretveit <janegil-staff@Mac.lan>
2026-01-18 16:14:28 +0100

    cron.js added

--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import app from "./app.js";
+import job from "./config/cron.js";
 import { ENV } from "./config/env.js";
 
 const PORT = ENV.PORT || 5001;
+
+if (ENV.NODE_ENV === "production") job.start();
 
 app.listen(PORT, () => {
   console.log(`Server is running on ${PORT}`);
